56f17e8a0683e75c03626c35e877e40ced6a2a8b
Yasaman-habibi <yasihabibi833@gmail.com>
2024-09-11 11:16:20 +0330

    Add files via upload

---
+++ Print.java
@@ -0,0 +1,12 @@
+package ir.freeland.spring.initialspring.xml;
+
+/**
+ * Simple output handler
+ *
+ * Used only for demonstration purposes :P
+ */
+public class Print {
+  public void doPrint(String value) {
+    System.out.println(value);
+  }
+}
